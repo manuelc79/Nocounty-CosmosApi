80f3440be11545196e277eb71a9c2d9582185d92
Ziko <manuelc79@gmail.com>
2024-07-19 22:05:18 -0300

    Cors Config

--- back-end/src/main/java/com/cosmos_api/Cosmos/API/configuration/CorsConfiguration.java
+++ back-end/src/main/java/com/cosmos_api/Cosmos/API/configuration/CorsConfiguration.java
@@ -2,9 +2,11 @@ package com.cosmos_api.Cosmos.API.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
